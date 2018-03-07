4b5d1b5629e5884c0756d405aebb7d0309edc56d
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-07 22:12:42 +0100

    tt

--- index.js
+++ index.js
@@ -18,32 +18,13 @@ app.get('/', function (req, res) {
 	res.send('Hello world, I am a chat bot')
 })
 
-app.get('/webhook', (req, res) => {
-
-  // Your verify token. Should be a random string.
-  let VERIFY_TOKEN = "<YOUR_VERIFY_TOKEN>"
-    
-  // Parse the query params
-  let mode = req.query['hub.mode'];
-  let token = req.query['hub.verify_token'];
-  let challenge = req.query['hub.challenge'];
-    
-  // Checks if a token and mode is in the query string of the request
-  if (mode && token) {
-  
-    // Checks the mode and token sent is correct
-    if (mode === 'subscribe' && token === VERIFY_TOKEN) {
-      
-      // Responds with the challenge token from the request
-      console.log('WEBHOOK_VERIFIED');
-      res.status(200).send(challenge);
-    
-    } else {
-      // Responds with '403 Forbidden' if verify tokens do not match
-      res.sendStatus(403);      
-    }
-  }
-});
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	if (req.query['hub.verify_token'] === 'myToken') {
+		res.send(req.query['hub.challenge'])
+	}
+	res.send('Error, wrong token')
+})
 
 // Spin up the server
 app.listen(app.get('port'), function() {
