863883595b683a209d52c1e3860daf49f110ad98
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-07 22:33:06 +0100

    t

--- index.js
+++ index.js
@@ -27,30 +27,13 @@ app.get('/webhook/', function (req, res) {
 })
 
 
-// Creates the endpoint for our webhook 
 app.post('/webhook/', function (req, res) {
- 
-  let body = req.body;
-
-  // Checks this is an event from a page subscription
-  if (body.object === 'page') {
-
-    // Iterates over each entry - there may be multiple if batched
-    body.entry.forEach(function(entry) {
-
-      // Gets the message. entry.messaging is an array, but 
-      // will only ever contain one message, so we get index 0
-      let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    });
-
-    // Returns a '200 OK' response to all requests
-    res.status(200).send('EVENT_RECEIVED');
-  } else {
-    // Returns a '404 Not Found' if event is not from a page subscription
-    res.sendStatus(404);
-  }
-
+    let messaging_events = req.body.entry[0].messaging
+    for (let i = 0; i < messaging_events.length; i++) {
+	    let event = req.body.entry[0].messaging[i]
+	    let sender = event.sender.id
+    }
+    res.send('EVENT_RECEIVED');
 })
 
 
