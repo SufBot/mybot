7a635e7187dbb975b3e944a9db2effb649044c70
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-08 00:04:31 +0100

    tt

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
+      "text": `You sent the message: "${received_message.text}". Now send me an image!`
     }
   } else if (received_message.attachments) {
     // Get the URL of the message attachment
