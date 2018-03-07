3ff1a6233f7546a462f658185b008f761eec2a65
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-07 22:21:42 +0100

    test

--- index.js
+++ index.js
@@ -24,7 +24,38 @@ app.get('/webhook/', function (req, res) {
 		res.send(req.query['hub.challenge'])
 	}
 	res.send('Error, wrong token')
-});
+})
+
+
+// Creates the endpoint for our webhook 
+app.post('/webhook/', function (req, res) {
+ 
+  let body = req.body;
+
+  // Checks this is an event from a page subscription
+  if (body.object === 'page') {
+
+    // Iterates over each entry - there may be multiple if batched
+    body.entry.forEach(function(entry) {
+
+      // Gets the message. entry.messaging is an array, but 
+      // will only ever contain one message, so we get index 0
+      let webhook_event = entry.messaging[0];
+      console.log(webhook_event);
+    });
+
+    // Returns a '200 OK' response to all requests
+    res.status(200).send('EVENT_RECEIVED');
+  } else {
+    // Returns a '404 Not Found' if event is not from a page subscription
+    res.sendStatus(404);
+  }
+
+})
+
+
+
+
 
 // Spin up the server
 app.listen(app.get('port'), function() {
