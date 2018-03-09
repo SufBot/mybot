53841f237b0cfa4577a4835b6447869a7e32c4b5
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-09 22:23:56 +0100

    t

--- index.js
+++ index.js
@@ -55,10 +55,11 @@ app.post('/webhook', (req, res) => {
 	  // Check if the event is a message or postback and
 	  // pass the event to the appropriate handler function
 	  if (webhook_event.message) {
-		handleMessage(sender_psid, webhook_event.message);        
-	  }
-	  if (webhook_event.message.quick_reply){
-		handleQuickReply(sender_psid, webhook_event.message.quick_reply);
+		  if (webhook_event.message.quick_reply){
+			handleQuickReply(sender_psid, webhook_event.message.quick_reply);
+		  }else{
+			handleMessage(sender_psid, webhook_event.message);
+			}
 	  }
 	  if (webhook_event.postback) {
 		handlePostback(sender_psid, webhook_event.postback);
