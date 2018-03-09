0c376840c9bc76a86d03fce8a55774d4500dea01
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-09 15:08:18 +0100

    t

--- index.js
+++ index.js
@@ -146,9 +146,9 @@ function callSendAPIGetName(sender_psid) {
     "method": "GET",
   }, (err, res, body) => {
     if (!err) {
-		response = { "text": "Bonjour "+body.first_name }
+		let response = { "text": "Bonjour "+body.first_name }
 	callSendAPI(sender_psid, response);
-      console.log('Got name')
+      console.log('Got a name !')
     } else {
       console.error("Unable to get name:" + err);
     }
