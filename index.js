04826b453d79bf3e421d7f58e5846eb705b38b8b
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-10 15:15:19 +0100

    Toto

--- index.js
+++ index.js
@@ -136,7 +136,7 @@ function callSendAPIGetName(sender_psid) {
     if (!err) {
 		let bodyObj = JSON.parse(body);
         let name = bodyObj.first_name;
-		let response = { "text":"Bonjour "+name+decodeURI(", \u000A Ti ta ti ti ! \u1F916")};
+		let response = { "text":"Bonjour "+name+decodeURI(", \u000A Ti ta ti ti ! \u23F3")};
 		callSendAPI(sender_psid, response);
     } else {
       console.error("Unable to get name:" + err);
