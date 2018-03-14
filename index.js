c581cacb277f5ef4bf8a56d23186bd495cd33fc9
Cuke7 <30923342+Cuke7@users.noreply.github.com>
2018-03-14 16:39:51 +0100

    Try to add rock climbing emoji

--- index.js
+++ index.js
@@ -260,7 +260,7 @@ function handleQuickReply(sender_psid, received_message) {
 			  },
 			  {
 				"content_type":"text",
-				"title":"Activité Spé \200D🛶",
+				"title":"Activité Spé \u200D🛶",
 				"payload":"activité"
 			  },
 			  {
